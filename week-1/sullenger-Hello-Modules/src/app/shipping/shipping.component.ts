46b064be9095888ecd10615b7922366a9a98d99d
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-07-10 20:10:24 -0600

    adding 1.5

---
+++ week-1/sullenger-Hello-Modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p style="text-align:center; color:blue; border: 1px solid black; height:200px">
+      Sully's shipping component
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
